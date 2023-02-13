8f845bf30c480ed9a1bdb5b6fe25dd4df45b4671
nicepowpowloko <90335368+nicepowpowloko@users.noreply.github.com>
2023-02-13 11:26:54 +0100

    13.02

--- js/background.js
+++ js/background.js
@@ -1,10 +1,5 @@
-$(document).ready(function()
-{
-  $("#boutons a").on('click', function(){
-    var idx = $(this).attr('id');
-    idx = idx.replace('bt','');
-    $('.fixed').not('#fixed'+idx).fadeOut(1000);
-    $('#fixed'+idx).fadeIn(1000);
-    $('#boutons').removeClass().addClass('bts'+idx);
-  });
+$('.patch').click(function(){
+  $(this).addClass('active');
+  $('.patch').not($(this)).removeClass('active');
+  $('body').attr('class',$(this).attr('class').split(' ')[1]);
 });
